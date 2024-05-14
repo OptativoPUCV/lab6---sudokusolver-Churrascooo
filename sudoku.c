238fce6a6ee58e92652ecdc1233efed8c3970546
Your Name <you@example.com>
2024-05-14 18:39:50 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -10,10 +10,6 @@ typedef struct{
 Node* createNode()
 {
    Node* n=(Node*) malloc(sizeof(Node));
-   if (n == NULL)
-   {
-      exit(EXIT_FAILURE);
-   }
    return n;
 }
 //-------------------------------------------------------------------
@@ -96,23 +92,27 @@ List* get_adj_nodes(Node* n)
    List* list=createList();
    int i, j, k;
    for (i = 0; i < 9; i++)
+   {
+      for (j = 0 ; j < 9; j++)
       {
-         for (j = 0 ; i < 9; j++)
+         if (n->sudo[i][j] == 0)
+         {
+            for (k = 1 ; k <= 9 ; k++)
             {
-               if (n->sudo[i][j] == 0)
+               Node *copia = copy(n);
+               copia->sudo[i][j] = k;
+               if (is_valid(copia))
                {
-                  for (k = 1 ; k <= 9 ; k++)
-                  {
-                     if (is_valid(n))
-                     {
-                        Node *copia = copy(n);
-                        copia->sudo[i][j] = k;
-                        pushBack(list, copia);
-                     }
-                  }
+                  pushBack(list, copia);
+               }
+               else
+               {
+                  free(copia);
                }
             }
+         }
       }
+   }
    return list;
 }
 //-------------------------------------------------------------------
