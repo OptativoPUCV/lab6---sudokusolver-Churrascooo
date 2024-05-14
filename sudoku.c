e35a232b1b74e95734af1dfe872b5fbd51616a05
Your Name <you@example.com>
2024-05-14 19:08:23 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -82,10 +82,9 @@ int is_valid(Node* n)
                      return 0;
                   }
                }
-                  }
-               }
             }
-         }
+        }
+      } 
    }
     return 1;
 }
